a2f3d0d8beb74b8d221bd939f797b4d87c6a7bb6
Nadav Levintov <dsp155@gmail.com>
2017-11-25 15:26:57 +0200

    Until 19

--- scriptsAndExperiments/temperature.py
+++ scriptsAndExperiments/temperature.py
@@ -7,7 +7,7 @@ X = np.array([400, 900, 390, 1000, 550])
 def _calc_sum(vector_x, t, vector_min):
     vector_sum = 0.0
     for x_i in vector_x:
-        vector_sum = vector_sum + pow((x_i / vector_min), (-1 / t))
+        vector_sum += pow((x_i / vector_min), (-1 / t))
     return vector_sum
 
 
@@ -18,14 +18,14 @@ def _cal_prob(x_i, alpha, t, vector_sum):
     return res
 
 
-range = np.linspace(0.01, 5, 100)
+t_range = np.linspace(0.01, 5, 100)
 alpha = X.min()
 for pnt in X:
     pnt_vector = []
-    for t in range:
+    for t in t_range:
         vector_sum = _calc_sum(X, t, alpha)
         pnt_vector.append(_cal_prob(pnt, alpha, t, vector_sum))
-    plt.plot(range, pnt_vector)
+    plt.plot(t_range, pnt_vector)
 
 plt.axis([0, 5, 0, 1])
 plt.legend(X)
